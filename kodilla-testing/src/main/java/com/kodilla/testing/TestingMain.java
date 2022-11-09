1177a795f31b50958093d7640ce1ac271f3abb07
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2022-11-09 23:09:46 +0100

    6.2 Added class Calculator. Made unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        // test 6.1 - Zadanie
+        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        // test 6.2 - przykład
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
         }
-    }
+
+        // test 6.2 - Zadanie: pierwszy test jednostkowy
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(51,88);
+        int resultSub = calculator.substract(9,33);
+        System.out.println("Test of add method: " + resultAdd);
+        System.out.println("Test of substract method: " + resultSub);
+        }
+}
 
 
