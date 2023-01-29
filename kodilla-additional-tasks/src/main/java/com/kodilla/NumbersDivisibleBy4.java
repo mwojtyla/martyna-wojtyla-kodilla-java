18208dcad4e26c01f1313c6c4c56b65a620192a7
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-01-29 10:28:12 +0100

    Added class: NumbersDivisibleBy4

---
+++ kodilla-additional-tasks/src/main/java/com/kodilla/NumbersDivisibleBy4.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class NumbersDivisibleBy4 {
+    public static void main(String[] args) {
+
+        System.out.println("Numbers divisible by 4 from the range (0-100): ");
+
+        for (int i = 0; i <= 100; i=i+4){
+                System.out.println(i);
+            }
+    }
+}
