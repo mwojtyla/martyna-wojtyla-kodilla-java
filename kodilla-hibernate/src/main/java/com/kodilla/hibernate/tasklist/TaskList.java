36a099b2ead4b772b987482abfe7e3ca444ab6d9
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-02-03 11:00:24 +0100

    17.3 Added classes: Invoice, Product, Item, Company, Employee, TaskFinancialDetails and classes Dao

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,7 +1,11 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="TASKLISTS")
@@ -9,6 +13,7 @@ public class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
@@ -17,7 +22,6 @@ public class TaskList {
         this.listName = listName;
         this.description = description;
     }
-
     @Id
     @GeneratedValue
     @NotNull
@@ -26,6 +30,7 @@ public class TaskList {
         return id;
     }
 
+    @NotNull
     @Column(name = "LISTNAME")
     public String getListName() {
         return listName;
@@ -35,6 +40,20 @@ public class TaskList {
     public String getDescription() {
         return description;
     }
+
+    @OneToMany(targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
 
     private void setId(int id) {
         this.id = id;
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,75 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private ItemDao itemDao;
+
+    @Test
+    void testInvoiceDaoSave() {
+        //Given
+        Product product1 = new Product("Cup for coffee");
+        Product product2 = new Product("Kettle Zelmer");
+        Product product3 = new Product("Shallow plate");
+
+        Item item1 = new Item(new BigDecimal(12), 10, new BigDecimal(120));
+        Item item2 = new Item(new BigDecimal(12), 10, new BigDecimal(120));
+        Item item3 = new Item(new BigDecimal(110), 1, new BigDecimal(110));
+        Item item4 = new Item(new BigDecimal(280), 1, new BigDecimal(280));
+
+        Invoice invoice = new Invoice("20230202/1");
+
+        /*product1.getItems().add(item1);
+        product1.getItems().add(item2);
+        product2.getItems().add(item3);
+        product3.getItems().add(item4);*/
+
+        item1.setProduct(product1);
+        item2.setProduct(product1);
+        item3.setProduct(product2);
+        item4.setProduct(product3);
+
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+        item3.setInvoice(invoice);
+        item4.setInvoice(invoice);
+
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        invoice.getItems().add(item3);
+        invoice.getItems().add(item4);
+
+
+        //When
+        invoiceDao.save(invoice);
+        int invoiceId = invoice.getId();
+
+        //Then
+        assertNotEquals(0, invoiceId);
+
+        //CleanUp
+        try {
+            invoiceDao.deleteById(invoiceId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+}
+
