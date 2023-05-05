417b09f1ab90890bda1f6315ac707ef85d1ad71d
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-05-05 22:11:00 +0200

    1.6 Added class Program

---
+++ kodilla-initial-tasks/src/main/java/com/kodilla/Program.java
@@ -0,0 +1,9 @@
+package com.kodilla;
+
+public class Program {
+    public static void main (String[] args) {
+        Program java = new Program();
+        int numbers = java.hashCode();
+        System.out.println(numbers);
+    }
+}
