03429954a92e65e5ddb2996bd7cf4a8cf211b5d0
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-05-15 16:58:37 +0200

    DecimalToHexadecimalConverter modified

--- kodilla-additional-tasks/src/main/java/com/kodilla/DecimalToHexadecimalConverter.java
+++ kodilla-additional-tasks/src/main/java/com/kodilla/DecimalToHexadecimalConverter.java
@@ -8,58 +8,16 @@ public class DecimalToHexadecimalConverter {
         List<String> list = new ArrayList<>();
         String hexadecimalNumbers = "";
         boolean value = true;
+        String hexadecimalValues = "0123456789ABCDEF";
 
         while (value) {
             int rest = number % 16;
             if (number > 0) {
-                if (rest == 0) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 1) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 2) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 3) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 4) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 5) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 6) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 7) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 8) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 9) {
-                    list.add(Integer.toString(rest));
-                    number = number / 16;}
-                if (rest == 10) {
-                    list.add("A");
-                    number = number / 16;}
-                if (rest == 11) {
-                    list.add("B");
-                    number = number / 16;}
-                if (rest == 12) {
-                    list.add("C");
-                    number = number / 16;}
-                if (rest == 13) {
-                    list.add("D");
-                    number = number / 16;}
-                if (rest == 14) {
-                    list.add("E");
-                    number = number / 16;}
-                if (rest == 15) {
-                    list.add("F");
-                    number = number / 16;
+                for (int i = 0; i < 16; i++) {
+                    if (rest == i) {
+                        list.add(String.valueOf(hexadecimalValues.charAt(i)));
+                        number = number / 16;
+                    }
                 }
             } else if (number == 0) {
                 value = false;
