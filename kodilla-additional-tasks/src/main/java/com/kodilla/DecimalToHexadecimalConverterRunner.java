cdfe894b10d9bab242a835c347e2c3f6a01b0d2f
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2023-05-15 07:12:36 +0200

    Added classes DecimalToHexadecimalConverter and DecimalToHexadecimalConverterRunner

---
+++ kodilla-additional-tasks/src/main/java/com/kodilla/DecimalToHexadecimalConverterRunner.java
@@ -0,0 +1,9 @@
+package com.kodilla;
+
+public class DecimalToHexadecimalConverterRunner {
+    public static void main(String[] args) {
+        DecimalToHexadecimalConverter decimalToHexadecimalConverter = new DecimalToHexadecimalConverter();
+        String result = decimalToHexadecimalConverter.converter(590);
+        System.out.println(result);
+    }
+}
